2f04398ea07bf0c2c6151692671c63204ef85231
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2025-06-02 21:27:02 +0700

    fix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,33 @@ import "react-slideshow-image/dist/styles.css";
 
 export const metadata: Metadata = {
   title: "Cahaya Bangun Perkasa | General Tranding and Contractor",
-  description: "Bahan bangunan murah dan berkualitas",
+  description:
+    "Cahaya Bangun Perkasa adalah perusahaan yang bergerak di bidang general trading dan kontraktor. Kami berkomitmen untuk memberikan pelayanan yang terbaik dan produk yang berkualitas untuk memenuhi kebutuhan pelanggan. Kami memiliki berbagai macam produk bahan bangunan yang lengkap dan harga yang terjangkau.",
+  metadataBase: new URL("https://cahayabangunperkasa.vercel.app"),
+  openGraph: {
+    title: "Cahaya Bangun Perkasa | General Tranding and Contractor",
+    description:
+      "Cahaya Bangun Perkasa adalah perusahaan yang bergerak di bidang general trading dan kontraktor. Kami berkomitmen untuk memberikan pelayanan yang terbaik dan produk yang berkualitas untuk memenuhi kebutuhan pelanggan. Kami memiliki berbagai macam produk bahan bangunan yang lengkap dan harga yang terjangkau.",
+    url: "https://cahayabangunperkasa.vercel.app",
+    siteName: "Cahaya Bangun Perkasa | General Tranding and Contractor",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Cahaya Bangun Perkasa | General Tranding and Contractor",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Cahaya Bangun Perkasa | General Tranding and Contractor",
+    description:
+      "Cahaya Bangun Perkasa adalah perusahaan yang bergerak di bidang general trading dan kontraktor. Kami berkomitmen untuk memberikan pelayanan yang terbaik dan produk yang berkualitas untuk memenuhi kebutuhan pelanggan. Kami memiliki berbagai macam produk bahan bangunan yang lengkap dan harga yang terjangkau.",
+    images: ["/og-image.png"],
+  },
 };
 
 const RootLayout = ({
